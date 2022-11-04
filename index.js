24cb1d07e30f22dc7b6d75e769aa2caf1a1302a4
Noob-H4cker <alsamiul123@gmail.com>
2022-11-04 15:39:27 +0600

    server setup

--- index.js
+++ index.js
@@ -1,21 +1,54 @@
-const express = require('express');
-const cors = require('cors');
+// const express = require('express');
+// const cors = require('cors');
+// const { MongoClient, ServerApiVersion } = require('mongodb');
+// require('dotenv').config()
+// const app = express();
+// const port = process.env.port || 5000;
+
+// //geniusDbUser
+// //hQcGOdVjE3PaGYRu
+
+// //middleware
+// app.use(cors());
+// app.use(express());
+
+// console.log(process.env.DB_USER)
+// console.log(process.env.DB_PASSWORD);
+
+// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.rqwof3p.mongodb.net/?retryWrites=true&w=majority`;
+// console.log(uri);
+// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+// client.connect(err => {
+//   const collection = client.db("test").collection("devices");
+//   // perform actions on the collection object
+//   client.close();
+// });
+
+
+// app.get('/',(req,res) => {
+//     res.send("Server Running");
+// })
+
+// app.listen(port,() => {
+//     console.log(`Server running on ${port}`);
+// })
+
+const express = require ('express');
+const cors = require ('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config()
 const app = express();
 const port = process.env.port || 5000;
 
-//geniusDbUser
-//hQcGOdVjE3PaGYRu
 
 //middleware
 app.use(cors());
-app.use(express());
+app.use(express.json());
 
-console.log(process.env.DB_USER)
+console.log(process.env.DB_USER);
 console.log(process.env.DB_PASSWORD);
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.rqwof3p.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://process.env.DB_USER:process.env.DB_PASSWORD@cluster0.rqwof3p.mongodb.net/?retryWrites=true&w=majority`;
 console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 client.connect(err => {
@@ -25,11 +58,11 @@ client.connect(err => {
 });
 
 
+
 app.get('/',(req,res) => {
-    res.send("Server Running");
+    res.send("Genius Server Running ");
 })
 
 app.listen(port,() => {
-    console.log(`Server running on ${port}`);
+    console.log(`Server Running on Port ${port}`);
 })
-
