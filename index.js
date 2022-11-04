e7eec11625e295d9b0c5d01ac4610ba1be70b3a3
Noob-H4cker <alsamiul123@gmail.com>
2022-11-04 15:25:20 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config()
+const app = express();
+const port = process.env.port || 5000;
+
+//geniusDbUser
+//hQcGOdVjE3PaGYRu
+
+//middleware
+app.use(cors());
+app.use(express());
+
+console.log(process.env.DB_USER)
+console.log(process.env.DB_PASSWORD);
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.rqwof3p.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
+
+app.get('/',(req,res) => {
+    res.send("Server Running");
+})
+
+app.listen(port,() => {
+    console.log(`Server running on ${port}`);
+})
+
