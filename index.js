4a0da5d6794b1640fe2568fc1f10472b46d23185
Noob-H4cker <alsamiul123@gmail.com>
2022-11-15 20:15:45 +0600

    post data created and get

--- index.js
+++ index.js
@@ -1,120 +1,115 @@
-// const express = require ('express');
-// const cors = require ('cors');
-// const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-// require('dotenv').config()
-// const app = express();
-// const port = process.env.port || 5000;
 
-// //middleware
+/*
+1. express,cors require
+2. express call 
+3. port
+4. express, cors use
+5. app.get and app.listen
+6. .env file create and DB_USER and DB_PASSWORD set
+7. dot env require 
+8. after middleware console DB_USER and DB_PASSWORD
+9. connect mongodb 
+10. wrap uri with template string and set user and password  dynamically with ${process.env.DB_USER}
+11. async function create and inside try block database and collection create 
+12. follow find multiple documents for all services
+13. find a document for single service
+14. data created by app.post in database and follow insert document
+15. then fetch data ,included fetch, 
+*/
 
-// app.use(cors());
-// app.use(express.json());
-
-// // console.log(process.env.DB_USER);
-// // console.log(process.env.DB_PASSWORD);
-
-
-// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.rqwof3p.mongodb.net/?retryWrites=true&w=majority`;
-// console.log(uri);
-// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-
-// async function run(){
-//   try{
-//     const serviceCollection = client.db('geniusCar').collection('services');
-//     app.get('/services',async(req,res) => {
-//       const  query ={};
-//       const cursor = serviceCollection.find(query);
-//       const services = await cursor.toArray();
-//       res.send(services);
-//     });
-
-//     app.get('/services/:id',async(req,res) => {
-//       const id = req.params.id;
-//       const query = {_id: ObjectId(id)}
-//       const services = await serviceCollection.findOne(query)
-//       res.send(services);
-//     })
-    
-//   }
-//   finally{
-
-//   }
-// }
-// run().catch(error => console.log(error.message));
-
-
-
-// app.get('/',(req,res) => {
-//   res.send('Server Running');
-// })
-
-// app.listen(port,() => {
-//   console.log(`Server Running On Port ${port}`);
-// })
-
-//1. express,cors require
-//2. express call 
-//3. port
-//4. express, cors use
-//5. app.get and app.listen
-//6. .env file create and DB_USER and DB_PASSWORD set
-//7. dot env require 
-//8. after middleware console DB_USER and DB_PASSWORD
-//9. connect mongodb 
-//10. wrap uri with template string and set user and password  dynamically with ${process.env.DB_USER}
-//11. async function create and inside try block database and collection create 
-//12. follow find multiple documents for all services
-//13. find a document for single service
 
 
 const express = require('express');
-const cors = require ('cors');
+const cors = require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
+const Port = process.env.Port || 5000;
+
 const app = express();
-const port = process.env.port || 5000;
 
 //middleware
-app.use(cors());
 app.use(express.json());
+app.use(cors());
+
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.rqwof3p.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run() {
   try{
     const serviceCollection = client.db('geniusCar').collection('services');
+    const orderCollection = client.db('geniusCar').collection('orders');
 
     app.get('/services',async(req,res) => {
-      const serviceCollection = client.db('geniusCar').collection('services');
       const query = {};
       const cursor = serviceCollection.find(query);
       const services = await cursor.toArray();
       res.send(services);
     })
 
-    app.get('/services/:id',async(req,res) => {
+    app.get('/services/:id',async(req,res) =>{
       const id = req.params.id;
-      const query = {_id: ObjectId(id)}
+      const query = {_id:ObjectId(id)};
       const service = await serviceCollection.findOne(query);
       res.send(service);
     })
 
-  }
+    app.get('/orders',async(req,res) => {
+      let query = {};
+      if(req.query.email){
+        query = {
+          email: req.query.email
+        }
+        const cursor = orderCollection.find(query);
+        const orders = await cursor.toArray();
+        res.send(orders);
+      }
+    })
 
+    app.post ('/orders',async(req,res) => {
+      const order = req.body;
+      const result = orderCollection.insertOne(order);
+      res.send(result);
+    })
+
+  
+  }
   finally{
-    
+
   }
 }
-run().catch(error => console.log(error.message));
+run().catch(error => console.error(error.message));
+
 
 
 app.get('/',(req,res) => {
-  res.send("Server Running");
+  res.send('Server Running');
 })
 
-app.listen(port,() => {
-  console.log(`Server Running on Port ${port}`);
+app.listen(Port,() => {
+  console.log(`Server Running on Port ${Port}`);
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
