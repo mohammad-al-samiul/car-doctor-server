e6733e5386427a6e67017e1c0ca2502b8da35aae
Noob-H4cker <alsamiul123@gmail.com>
2023-02-16 18:42:36 +0600

    final commit

--- index.js
+++ index.js
@@ -98,48 +98,7 @@ async function run() {
       res.send(result)
     })
 
-    // app.get('/services',async(req,res) => {
-    //   const query = {};
-    //   const cursor = serviceCollection.find(query);
-    //   const services = await cursor.toArray();
-    //   res.send(services);
-    // })
 
-    // app.get('/services/:id',async(req,res) =>{
-    //   const id = req.params.id;
-    //   const query = {_id:ObjectId(id)};
-    //   const service = await serviceCollection.findOne(query);
-    //   res.send(service);
-    // })
-
-    // app.get('/orders',async(req,res) => {
-    //   let query = {};
-    //   if(req.query.email){
-    //     query = {
-    //       email: req.query.email
-    //     }
-    //     const cursor = orderCollection.find(query);
-    //     const orders = await cursor.toArray();
-    //     res.send(orders);
-    //   }
-    // })
-
-    // app.post ('/orders',async(req,res) => {
-    //   const order = req.body;
-    //   const result = orderCollection.insertOne(order);
-    //   res.send(result);
-    // })
-
-    // app.delete('/orders/:id', async(req,res) => {
-    //   const id = req.params.id;
-    //   const query = { _id:ObjectId(id)};
-    //   const result = await orderCollection.deleteOne(query);
-    //   res.send(result);
-    // })
-
-    
-
-   
 
   
   }
